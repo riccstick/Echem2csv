7ed74616a5eb443a2e23fdcdb333a1aaef0fddbb
riccstick <44583450+riccstick@users.noreply.github.com>
2020-12-19 22:57:07 +0100

    Update to v1.2
    - improvements (more unit options)
    - Gui script added

--- Echem2csv.py
+++ Echem2csv.py
@@ -1,4 +1,4 @@
-#!/home/ricci/anaconda3/bin/python3
+#!/usr/bin/python3
 
 import argparse, textwrap
 import pandas as pd
@@ -8,18 +8,23 @@ parser = argparse.ArgumentParser(
 	formatter_class=argparse.RawDescriptionHelpFormatter,
 	description=textwrap.dedent('''\
  +----------------+
- | Echem2csv_v1.1 |
+ | Echem2csv_v1.2 |
  +----------------+
  by Erik Breslmayr, 2020
  
- - This program reads .csv files containing two columns.
- - Column A -> Potential (V); Column B -> Current (A)
+ Echem2csv reads .csv files containing two columns.
+
+ Input Sample:
+ Column1         Column2 
+ Potential (V)   Current (A)
  
- - The Potential is converted into mV and can be converted to SHE (Standard Hydrogen Electrode) values.
- - The Current is converted into µA or nA, however a random factor can also be applied.
+ The Potential is converted into a unit specified and can be converted to SHE (Standard Hydrogen Electrode) values. The Current is converted into a unit specified, or a random factor can also be applied.
  
- - Finally a file is created and all Current columns are combined and saved. The xAxis or first column with the Potential (mV) is choosen from the first file processed!
- - Column A -> Potential (mV); Column B -> Current (µA); Column nth...
+ Finally a file is created and all Current columns are combined and saved. The columns header is chosen from the filename. The xAxis or first column with the Potential (mV) is chosen from the first file processed!
+
+ Output Sample:
+ Column 1                           Column2     Column*
+ Potential (mV) / Current (µA)      filename1   filename*
 
  - Examples:
     Echem2csv.py -i file1.csv file2.csv 
@@ -28,14 +33,18 @@ parser = argparse.ArgumentParser(
     
 '''))
 
-parser.add_argument("-i", "--infile", nargs='+', required=True, help='Specify input filename in csv format!')
-parser.add_argument("-o", "--outfile", default="processedFile.csv", help='Optional: Specify output filename')
+parser.add_argument("-i", "--Inputfiles", nargs='+', required=True, help='Specify input filenames in csv format!')
+parser.add_argument("-o", "--Outputfile", default="Echem2Output.csv", help='Optional: Specify output filename')
 
 parser.add_argument("-she", "--SHE_convert", default="200", help='Specify factor to convert to Standard Hydrogen Electrode potential; default = 200')
 
+parser.add_argument("-pot", "--potential_convert", default="mV", help='Specify Unit (V, mV, µV, nV, pV; default = mV')
+
 parser.add_argument("-cur", "--current_convert", default="µA", help='Choose if converted to µA or nA; default = µA')
 
-parser.add_argument("-curValue", "--current_convert_value", help='Specify a factor to convert e.g. 10')
+parser.add_argument("-potValue", "--potential_convert_value", help='Random factor; e.g. 123')
+
+parser.add_argument("-curValue", "--current_convert_value", help='Specify a factor to convert e.g. 123')
 
 parser.add_argument("-s", "--seperator", default=",", help='Optional: Choose seperator (e.g. tab); default is a comma ,')
 
@@ -44,45 +53,80 @@ args = parser.parse_args()
 if args.seperator == "tab":
 	args.seperator = "\t"
 
-if args.current_convert == "µA":
-    curFactor = 1000000
-    colName = 'Current (µA)'
-elif args.current_convert == "nA":
-    curFactor = 1000000000
-    colName = 'Current (nA)'
-else:
-    curFactor = args.curValue
-    colName = 'Current (custom)'
+def potCalc():
+    if args.potential_convert == "V":
+        xcolFactor = 1
+        xcolName = 'Potential (V) / '        
+    elif args.potential_convert == "mV":
+        xcolFactor = 1000
+        xcolName = 'Potential (mV) / '        
+    elif args.potential_convert == "µV":
+        xcolFactor = 1000000
+        xcolName = 'Potential (µV) / '
+    elif args.potential_convert == "nV":
+        xcolFactor = 1000000000
+        xcolName = 'Potential (nV) / '
+    elif args.potential_convert == "pV":
+        xcolFactor = 1000000000000
+        xcolName = 'Potential (pV) / '
+    else:
+        xcolFactor = args.potential_convert_value
+        xcolName = 'Potential (custom)'
+    return xcolFactor, xcolName
+        
+
+def curCalc():
+    if args.current_convert == "A":
+        ycolFactor = 1
+        ycolName = 'Current (A)'        
+    elif args.current_convert == "mA":
+        ycolFactor = 1000
+        ycolName = 'Current (mA)'        
+    elif args.current_convert == "µA":
+        ycolFactor = 1000000
+        ycolName = 'Current (µA)'
+    elif args.current_convert == "nA":
+        ycolFactor = 1000000000
+        ycolName = 'Current (nA)'
+    elif args.current_convert == "pA":
+        ycolFactor = 1000000000000
+        ycolName = 'Current (pA)'
+    else:
+        ycolFactor = args.current_convert_value
+        ycolName = 'Current (custom)'
+    return ycolFactor, ycolName
 
 def xcolumn():
-    with open(args.infile[0], 'r') as f:     
+    with open(args.Inputfiles[0], 'r') as f:     
         data = pd.read_csv(f, sep=',', header=1, names = ['Potential', 'Current'])
-        xcolName = 'Potential (mV) / ' + colName
-        data[xcolName] = data['Potential'] * 1000 + float(args.SHE_convert)
-        x = data[xcolName]
+        xcolNameNew = xpot[1] + ycur[1]
+        data[xcolNameNew] = data['Potential'] * float(xpot[0]) + float(args.SHE_convert)
+        x = data[xcolNameNew]
         return x
     
 def ycolumns(f):
     with open(f, 'r') as f:     
         data = pd.read_csv(f, sep=',', header=1, names = ['Potential','Current'])
-        data[colName] = data['Current'] * float(curFactor)
-        y = data[colName]
+        data[ycur[1]] = data['Current'] * float(ycur[0])
+        y = data[ycur[1]]
         return y
-
+    
+xpot = potCalc()
+ycur = curCalc()
 xycombo = xcolumn()
 
-for i in enumerate(args.infile):
+for i in enumerate(args.Inputfiles):
     y = ycolumns(i[1])
     name = i[1].split('.csv')
     y = pd.concat([y], keys=[name[0]], axis=1)
     num = i[0] + 1
     xycombo = pd.concat([xycombo, y], axis=1)
     
-xycombo.to_csv(args.outfile, sep=args.seperator, index=False)
+xycombo.to_csv(args.Outputfile, sep=args.seperator, index=False)
 
 print("+-------------------------------------------+")
 print("   Processed and combined " + str(num) + " files.")
-print("   Output filename: " + args.outfile)
+print("   Output filename: " + args.Outputfile)
 print("+-------------------------------------------+")
 
     
--- README.md
+++ README.md
@@ -1,14 +1,49 @@
 # Echem2csv
-- python3 script to combine csv data
+- python3 script to combine CSV files
 ## Required modules
+
+### Command-line
+
 - argparse, textwrap, pandas
 
+### GUI 
+
+- gooey, textwrap, pandas
+
+## Installation
+
+### Command-line
+
+- Execute with python3: `python Echem2csv.py -arguments`
+- or change the path to your python3 location in the first line of the script and make it executable (`chmod +x Echem2csv.py`) in bash to run it directly from the shell: `Echem2csv.py -arguments`
+
+### GUI
+
+- Install [Gooey](https://github.com/chriskiehl/Gooey#how-does-it-work) and execute with python3: `python Echem2csvGui.py`
+
+<img src="./Gui.png" style="zoom:50%;" />
+
+
+
 ## Description
-- This program reads .csv files containing two columns.
-- Column A -> Potential (V); Column B -> Current (A)
+- Echem2csv reads CSV files containing two columns.
 
-- The Potential is converted into mV and can be converted to SHE values.
-- The Current is converted into µA or nA, however a random factor can also be choosen.
+  Input Sample:
 
-- Finally a file is created and all Currents are combined and saved.
-- Column A -> Potential (mV); Column B -> Current (µA); Column nth...
+  | Potential (V) | Current (A) |
+  | ------------- | ----------- |
+  | 0.00123       | 0.00000123  |
+
+  
+
+ - The Potential is converted into mV and can be converted to SHE (Standard Hydrogen Electrode) values. The Current is converted into µA or nA, however a random factor can also be applied.
+
+ - Finally a file is created and all Current columns are combined and saved. The columns header is chosen from the filename. The xAxis or first column with the Potential (mV) is chosen from the first file processed!
+
+ - Output Sample:
+
+   | Potential (mV) / Current (µA) | filename1 | filename* | …    |
+   | ----------------------------- | --------- | --------- | ---- |
+   | 1.23                          | 1.23      | 1.23      | …    |
+
+     
